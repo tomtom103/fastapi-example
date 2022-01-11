268106b72b2cfc3c53f4f2a26650d78aadf6983a
Thomas Caron <thomas.caron00@gmail.com>
2022-01-11 18:21:49 -0500

    Added multiprocessing

--- app/main.py
+++ app/main.py
@@ -1,11 +1,26 @@
+import asyncio
+import json
+from concurrent.futures.process import ProcessPoolExecutor
 from typing import Optional
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
-# WARNING: Cannot use async if calling another service (database, API, file system, etc.)
+async def run_in_process(fn, *args):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(app.state.executor, fn, *args) # wait and return result
 
+@app.on_event("startup")
+async def startup():
+    app.state.executor = ProcessPoolExecutor()
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    app.state.executor.shutdown()
+
+
+# WARNING: Cannot use async if calling another service (database, API, file system, etc.)
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
@@ -13,3 +28,9 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/async-endpoint")
+async def test_endpoint():
+    random_args = { "a": 1, "b": 2, "c": 3 }
+    res = await run_in_process(lambda x: f"Hello from async endpoint, received: {json.dumps(x)}", random_args)
+    return {"result": res}
